b73df724e7697da73d48ee30655fd9aa23af49a1
Owais-Khan-ICD2O1-01 <owaisali.khan@student.tdsb.on.ca>
2024-09-16 14:12:01 +0000

    Answer

--- PP1_2.py
+++ PP1_2.py
@@ -1,21 +1,46 @@
-
+'''
+    Lesson: Variables and Data Types
+    Author: Owais Ali Khan
+    Date Created: Sept 16, 2024
+    Date Last Modified: Sept 16, 2024
+'''
 
 #Write Assignment code here
 
 def q1():
+    name = "Alex"
+    print(name)
 
 def q2():
+    num1 = 3
+    num2 = 5
+    num3 = 2
+    print(num1 + num2 + num3)
 
 def q3():
+   first = 21
+   second = 4
+   print(first // second) 
 
 def q4():
+    num4 = 6.3
+    num5 = 3.0
+    print(num4 / num5)
 
 def q5():
+    helloWorld = "Hello World"
+    print(helloWorld)
+    helloWorld = "Goodbye World"
+    print(helloWorld)
 
 def q6():
+    true = True
+    print(true)
 
 def q7():
-
+    num6 = 7
+    num7 = 2
+    print(num6 % num7)
 q1()
 q2()
 q3()
